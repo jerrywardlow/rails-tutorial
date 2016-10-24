f7d99e1851467b7af8421f0b026880244210908f
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-24 16:13:44 -0700

    Add first and last name attributes

--- sample_app/example_user.rb
+++ sample_app/example_user.rb
@@ -1,12 +1,17 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :name_first, :name_last, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    @name_first  = attributes[:name_first]
+    @name_last = attributes[:name_last]
     @email = attributes[:email]
   end
 
+  def full_name
+    "#{@name_first} #{@name_last}"
+  end
+
   def formatted_email
-    "#{@name} <#{@email}>"
+    puts full_name + " <#{@email}>"
   end
 end
